a88c9fc39188e0846b32c986de2cc12c94276817
Rehan-programer <rhnmhr786@gmail.com>
2025-09-19 19:31:55 +0500

    ui-set

--- src/app/women/page.js
+++ src/app/women/page.js
@@ -1,11 +1,8 @@
-import Card from "../Commponents/Card/Card";
 import CategoriesCard from "../Commponents/Categories/categoriesCard";
-import Women from "../Commponents/Categories/women/Women";
 import { WomenData } from "../Commponents/Categories/women/WomenData";
 export default function Womens() {
   return (
     <>
-      {/* <Women products={WomenData}/> */}
       <CategoriesCard data={WomenData}/>
     </>
   );
