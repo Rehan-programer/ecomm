63e3df7436c90eb3618d0feb423178fe1686081e
Rehan-programer <rhnmhr786@gmail.com>
2025-10-16 18:08:12 +0500

    .env update

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,9 @@
-
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', 
-  database: 'ecommerce',
+  host: process.env.MYSQL_HOST || 'mysql.railway.internal',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'KaacpakIEqLMFvZpHxQZpgEfAjtvHIsJ',
+  database: process.env.MYSQL_DATABASE || 'railway',
+  port: process.env.MYSQL_PORT || 3306,
 });
-
-
